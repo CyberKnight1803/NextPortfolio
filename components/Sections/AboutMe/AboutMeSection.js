c1adade3f0916c502f41cff13dc703b05b2a7b31
CyberKnight1803 <omkarpitale2001@gmail.com>
2022-06-14 01:10:58 +0530

    minor ui changes

--- components/Sections/AboutMe/AboutMeSection.js
+++ components/Sections/AboutMe/AboutMeSection.js
@@ -24,7 +24,7 @@ const AboutMeSection = (props) => {
                 <div className="grid grid-cols-6 relative w-3/4 tablet:mr-0 tablet:w-3/4 p-4 tablet:p-4 tablet:pl-16 ml-8 relative">
                     <img className="row-span-full col-start-1 col-span-4 rounded-3xl object-contain z-10" src="/pics/profile.png" alt="Omkar Profile PIC">
                     </img>
-                    <div className="row-span-full col-start-2 col-span-5 w-4/5 pl-8 p-4 mt-12 h-2/3 tablet:h-full tablet:mt-8 border-green-200 border-4 rounded-2xl ">
+                    <div className="row-span-full col-start-2 col-span-5 w-4/5 pl-8 p-4 mt-16 h-2/3 tablet:h-full tablet:mt-8 border-green-200 border-4 rounded-2xl ">
 
                     </div>
                 </div>
