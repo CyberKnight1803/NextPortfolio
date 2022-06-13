ebf8d392cadafa332abb3578361c083e06468831
CyberKnight1803 <omkarpitale2001@gmail.com>
2022-06-13 18:52:32 +0530

    resume button link bug solved

--- components/NavBar/ResumeButton.js
+++ components/NavBar/ResumeButton.js
@@ -2,14 +2,12 @@ import Link from "next/link";
 
 const ResumeButton = () => {
     return (
-        <div className="text-sm text-green-200 mt-1 mb-2 py-2  px-1 font-roboto border border-green-200 rounded-md hover:bg-green-100/25">
-                <a className="mx-4" href="https://drive.google.com/file/d/1ikw8eqT7JnzamDnH0MpuEYcgzcA3gAgD/view?usp=sharing" 
-                target="_blank"
-                rel="noreferrer"
-                >
-                    Resume
-                </a>
-        </div>
+        <a className="mx-4 text-sm text-green-200 mt-1 mb-2 py-2  px-4 font-roboto border border-green-200 rounded-md hover:bg-green-100/25" href="https://drive.google.com/file/d/1ikw8eqT7JnzamDnH0MpuEYcgzcA3gAgD/view?usp=sharing" 
+        target="_blank"
+        rel="noreferrer"
+        >
+            Resume
+        </a>
     );
 };
 
