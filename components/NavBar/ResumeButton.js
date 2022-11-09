864d24a26db9d2dad5169f870a3e79768c2b5bc6
CyberKnight1803 <omkarpitale2001@gmail.com>
2022-11-09 14:18:26 +0530

    update resume

--- components/NavBar/ResumeButton.js
+++ components/NavBar/ResumeButton.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const ResumeButton = () => {
     return (
-        <a className="mx-4 text-sm text-green-200 mt-1 mb-2 py-2  px-4 font-roboto border border-green-200 rounded-md hover:bg-green-100/25" href="https://drive.google.com/file/d/1ikw8eqT7JnzamDnH0MpuEYcgzcA3gAgD/view?usp=sharing" 
+        <a className="mx-4 text-sm text-green-200 mt-1 mb-2 py-2  px-4 font-roboto border border-green-200 rounded-md hover:bg-green-100/25" href="https://drive.google.com/file/d/14CgD4i1ziEThl6B7v-BF2Qwe24exrd8B/view?usp=sharing" 
         target="_blank"
         rel="noreferrer"
         >
